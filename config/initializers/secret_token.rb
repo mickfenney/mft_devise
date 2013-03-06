a495a12769cb702c845b1c4110fec09063b78718
mick-asoftware <mick@asoftware.net.au>
2013-03-06 22:06:57 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MftDevise::Application.config.secret_token = '37b0359c4996e7b0cfea6db449de3896fd1c3565f44f4b2451a6f05cc554c8d8307f5658d07621b87eb9456ff958f2b1708d43d9c73b45ca8b95092324092e24'
