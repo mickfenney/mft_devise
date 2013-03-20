068d45531415959bacb9f4f110b707ec820a791b
Mick Fenney <mick@asoftware.net.au>
2013-03-20 14:20:50 +1100

    Added themes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :set_theme
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
+
+  private
+  	def set_theme
+      @css_theme = 'default'
+      # unless current_user.nil
+      # 	@css_theme = (current_user.theme.nil ? 'default' : current_user.theme)
+      # end
+  	end
 
 end
