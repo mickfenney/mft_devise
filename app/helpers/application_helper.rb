47b9adee85c41209dbb169996b3fb9c9ea2880fc
mick-asoftware <mick@asoftware.net.au>
2013-03-27 21:58:03 +1100

    Updtes to the user edit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,7 +18,7 @@ module ApplicationHelper
     end
   end 
 
-    def link_to_add_fields(name, f, path, association)
+  def link_to_add_fields(name, f, path, association)
     new_object = f.object.send(association).klass.new
     id = new_object.object_id
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
