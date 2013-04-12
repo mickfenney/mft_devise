14be19e39fa02b4dcc34db634e47795b863b8c88
mick-asoftware <mick@asoftware.net.au>
2013-04-13 05:19:20 +1000

    Updated the contact us email to be html

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,7 +4,10 @@ class ContactMailer < ActionMailer::Base
 
   def send_contact_message(message)
     @message = message
-    mail subject: "mfTechnology Contact Message", from: message.email
+    mail(subject: "mfTechnology Contact Message", from: message.email) do |format|
+      format.html
+      #format.text
+    end
   end
 
 end
