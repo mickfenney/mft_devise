204b8657f0e8550a7fc21838f4ea8e3b15fb2427
mick-asoftware <mick@asoftware.net.au>
2013-04-18 03:14:09 +1000

    Updates to notification

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,15 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(params[:message])
     if @message.valid?
-      ContactMailer.send_contact_message(@message).deliver
+      email_args = {
+        template_data: @message, 
+        template_file: '_templates/email/send_contact_us', 
+        to: ENV["SITE_EMAIL"], 
+        subject: ENV["SITE_NAME"]+" Contact Us Message",
+        from: @message.email,
+        name: @message.name
+      }
+      Notification.send_email(email_args).deliver
       redirect_to root_url, notice: "Message sent! Thank you for contacting us."
     else
       render "new"
