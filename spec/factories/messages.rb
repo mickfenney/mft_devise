9211e5e1c4ff27bded395f6c89404870bf63e626
mick-asoftware <mick@asoftware.net.au>
2013-04-18 21:40:07 +1000

    Removes file for notification refactoring

--- spec/factories/messages.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :message do
-    name "From User"
-    email "from@example.com"
-    content "This is a test message for "+ENV["SITE_NAME"]
-  end
-end
