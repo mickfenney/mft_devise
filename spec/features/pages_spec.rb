f948cc5805fc9416a6e9e8fc3fb7eb7ff6e4e540
Mick Fenney <mick@asoftware.net.au>
2013-05-15 01:29:02 +1000

    Added more user and pages spec features tests

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -4,17 +4,25 @@ describe "Pages" do
 
   subject { page }
 
+  describe "Sign In page" do
+    before { visit "#{root_path}users/sign_in" }
+    it { page.should have_selector('h3', text: 'Sign In') }
+  end   
+  
+  describe "Sign Up page" do
+    before { visit "#{root_path}users/sign_up" }
+    it { page.should have_selector('h3', text: 'Sign Up') }
+  end    
+
   describe "Contact page" do
     before { visit contact_path }
-    
     it { should have_selector('h3', text: 'Contact Us') }
   end  
 
   describe "About page" do
     before { visit about_path }
-
     it { should have_selector('h3', text: 'About Us') }
-  end
+  end 
 
   it "should have the right links on the layout" do
     visit root_path
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,21 +1,45 @@
 require 'spec_helper'
 
-describe "User" do 
-
-  subject { page }
+describe "User Sign In" do 
 
   before(:each) do
-    @user = FactoryGirl.build(:user)
+    @user = FactoryGirl.create(:user)
   end
 
-  describe "Sign In" do
-    before { visit "#{root_path}users/sign_in" }
-    
-    it { should have_selector('h3', text: 'Sign In') }
-  end 
-
-  it "should be able to login with valid user" do
-  	pending
+  it "should be able to login as a valid user" do
+    visit "#{root_path}users/sign_in"
+    fill_in "Email", :with => @user.email
+    fill_in "Password", :with => @user.password
+    click_button "Sign in"
+    page.should have_content("Signed in successfully.")
+    page.should have_content(@user.name)
   end 	
+
+  it "should not be able to login as a non valid user" do
+    visit "#{root_path}users/sign_in"
+    fill_in "Email", :with => nil
+    fill_in "Password", :with => nil
+    click_button "Sign in"
+    page.should have_content("Invalid email or password.")
+  end   
+
+  it "should redirect to Edit User page when clicking Edit link on User page" do
+    visit "#{root_path}users/sign_in"
+    fill_in "Email", :with => @user.email
+    fill_in "Password", :with => @user.password
+    click_button "Sign in"
+    page.should have_content("Signed in successfully.")
+    page.should have_content(@user.name)
+    click_link @user.name
+    page.should have_selector('h3', text: 'User')
+    page.should have_content("Edit")
+    click_link "Edit"
+    page.should have_selector('h3', text: "Edit User: #{@user.name}")
+    page.should have_content("General")
+    page.should have_content("Profile Image")
+    page.should have_content("Locations")
+    page.should have_content("Password")
+    page.should have_content("Cancel my Account")
+  end    
 
 end	
