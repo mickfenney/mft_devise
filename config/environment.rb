fa02c83559365a31c66820f3607b7650b1aaa297
mick <mick@asoftware.net.au>
2013-06-19 01:25:10 +1000

    Added date formats to the environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 MftDevise::Application.initialize!
 
+# Date formats
+Time::DATE_FORMATS[:human]       = "%A, %e %B %Y"
+Time::DATE_FORMATS[:human_short] = "%a, %d %b %Y"
+
 require 'will_paginate'
