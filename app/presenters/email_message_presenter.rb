c36bdb9460ffb6e1eb82696cc1d80043de0047bb
unknown <fenneym@RAVX106872.fxdms.net>
2013-07-05 15:59:16 +1000

    Updated the email_message_presenter.rb

--- app/presenters/email_message_presenter.rb
+++ app/presenters/email_message_presenter.rb
@@ -2,11 +2,7 @@ class EmailMessagePresenter
 
   include ActiveAttr::Model
   
-  attribute :name
-  attribute :email
-  attribute :content
-  
-  attr_accessible :name, :email, :content
+  attr_accessor :name, :email, :content
 
   validates_presence_of :name
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
