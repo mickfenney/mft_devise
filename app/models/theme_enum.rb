c720ad98bb1a6f9adbd9f77f76a8ce84d01ba183
unknown <fenneym@RAVX106872.fxdms.net>
2013-07-16 09:47:17 +1000

    Added 7 new themes

--- app/models/theme_enum.rb
+++ app/models/theme_enum.rb
@@ -1,10 +1,17 @@
 class ThemeEnum < ActiveEnum::Base
   order :asc
 
-  value id: 'default', name: 'Default'
   value id: 'amelia', name: 'Amelia'
-  value id: 'slate', name: 'Slate'
-  value id: 'united', name: 'United'
   value id: 'cosmo', name: 'Cosmo'
+  value id: 'default', name: 'Default'
+  value id: 'cyborg', name: 'Cyborg'
+  value id: 'flatly', name: 'Flatly'
+  value id: 'journal', name: 'Journal'
+  value id: 'readable', name: 'Readable'
+  value id: 'simplex', name: 'Simplex'
+  value id: 'slate', name: 'Slate'
+  value id: 'spacelab', name: 'Spacelab'
+  value id: 'superhero', name: 'Superhero'
+  value id: 'united', name: 'United'
 
 end
