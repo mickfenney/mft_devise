465f95d15d7d7048c998c9340b1a4c93e9470447
mick-asoftware <mick@asoftware.net.au>
2013-07-19 10:30:11 +1000

    removed match from routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ MftDevise::Application.routes.draw do
 
   get "sessions/new"
 
-  match '/about', to: 'pages#about'
+  get '/about', to: 'pages#about'
 
   get  '/contact', to: 'contact_us#new'
   post '/contact', to: 'contact_us#create', :as => :contact_us
@@ -17,6 +17,6 @@ MftDevise::Application.routes.draw do
   	                                   :invitations   => 'invitations' }
   resources :users
 
-  match "/delayed_job" => DelayedJobWeb, :anchor => false
+  get "/delayed_job" => DelayedJobWeb, :anchor => false
 
 end
