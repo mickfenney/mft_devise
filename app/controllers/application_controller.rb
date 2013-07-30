e4dabb1e7f8ced959983d2b86c249d84e5117a3c
mick <mick@asoftware.net.au>
2013-07-31 01:40:39 +1000

    Added Auto Time Zone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :set_theme
+  before_filter :set_theme, :set_time_zone 
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
 
   private
+
   	def set_theme
       @css_theme = 'default'
       if user_signed_in?
       	@css_theme = current_user.theme ? current_user.theme : 'default'
       end
   	end
+
+    def set_time_zone
+      Time.zone = cookies["time_zone"]
+    end  
 
 end
