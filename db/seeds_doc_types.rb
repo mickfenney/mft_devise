1bbd14d071d1868028365135228983ac2436f6ab
mick-asoftware <mick@asoftware.net.au>
2013-08-01 17:33:28 +1000

    Updates to the doc seeds

--- db/seeds_doc_types.rb
+++ db/seeds_doc_types.rb
@@ -14,4 +14,60 @@ if DocumentType.count == 0
   )
   puts '+ doc type: ' << dt.name
 
+  dt = DocumentType.create(
+    :name => 'Java',
+    :description => 'Java (programming language)',
+    :user_id => 1,
+  )
+  puts '+ doc type: ' << dt.name
+
+  dt = DocumentType.create(
+    :name => 'Ruby',
+    :description => 'Ruby (programming language)',
+    :user_id => 1,
+  )
+  puts '+ doc type: ' << dt.name
+
+  dt = DocumentType.create(
+    :name => 'HTML',
+    :description => 'Hypertext Markup Language',
+    :user_id => 1,
+  )
+  puts '+ doc type: ' << dt.name
+
+  dt = DocumentType.create(
+    :name => 'MySQL',
+    :description => 'MySQL is a relational database management system (RDBMS)',
+    :user_id => 1,
+  )
+  puts '+ doc type: ' << dt.name  
+
+  dt = DocumentType.create(
+    :name => 'PL/SQL',
+    :description => 'Procedural Language/Structured Query Language (PL/SQL)',
+    :user_id => 1,
+  )
+  puts '+ doc type: ' << dt.name
+
+  dt = DocumentType.create(
+    :name => 'Javascript',
+    :description => 'JavaScript (JS) is an interpreted computer programming language',
+    :user_id => 1,
+  )
+  puts '+ doc type: ' << dt.name
+
+  dt = DocumentType.create(
+    :name => 'Rails',
+    :description => 'Ruby on Rails, is an open source web application framework',
+    :user_id => 1,
+  )
+  puts '+ doc type: ' << dt.name  
+
+  dt = DocumentType.create(
+    :name => 'PHP',
+    :description => 'PHP: Hypertext Preprocessor',
+    :user_id => 1,
+  )
+  puts '+ doc type: ' << dt.name   
+
 end
--- db/seeds_docs.rb
+++ db/seeds_docs.rb
@@ -2,7 +2,7 @@ if Document.count == 0
 
   doc = Document.create(
     :title => 'text doc',
-    :document_type_ids => [1,2],
+    :document_type_ids => [1],
     :body => 'This is a test document...',
     :user_id => 1,
   )
@@ -10,10 +10,10 @@ if Document.count == 0
 
   doc = Document.create(
     :title => 'perl doc',
-    :document_type_ids => [1,2],
+    :document_type_ids => [2],
     :body => 'This is a new perl document...',
     :user_id => 1,
   )
   puts '+ doc: ' << doc.title
 
-end  
+end
