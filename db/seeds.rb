c75a06858d18577761b5f5797f75e686f99ace70
mick <mick@asoftware.net.au>
2013-08-04 00:45:30 +1000

    Added a second admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,14 +14,27 @@ load "#{Rails.root}/db/seeds_roles.rb"
 
 puts '+ DEFAULT USERS'
 
-location1 = Location.create(:address => '75 Seaview Avenue, Safety Beach VIC 3936, Australia', :name => 'Home')
-location2 = Location.create(:address => '213 Robinsons Rd, Ravenhall Victoria 3023', :name => 'Work')
+location1 = Location.create(:address => ENV['ADMIN_LOCATION1'].dup, :name => 'Home')
+location2 = Location.create(:address => ENV['ADMIN_LOCATION2'].dup, :name => 'Work')
 user = User.new(
   :name => ENV['ADMIN_NAME'].dup,
   :email => ENV['ADMIN_EMAIL'].dup,
   :password => ENV['ADMIN_PASSWORD'].dup,
   :password_confirmation => ENV['ADMIN_PASSWORD'].dup,
   :locations => [location1, location2]
+)
+puts '+ user: ' << user.name
+user.skip_confirmation!
+user.add_role :admin
+user.save!
+
+location3 = Location.create(:address => ENV['ADMIN_LOCATION3'].dup, :name => 'Home')
+user = User.new(
+  :name => ENV['ADMIN_NAME2'].dup,
+  :email => ENV['ADMIN_EMAIL2'].dup,
+  :password => ENV['ADMIN_PASSWORD2'].dup,
+  :password_confirmation => ENV['ADMIN_PASSWORD2'].dup,
+  :locations => [location3]
 )
 puts '+ user: ' << user.name
 user.skip_confirmation!
