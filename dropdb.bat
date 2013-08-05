4b9fd572a8610fe23b7524b8effdae3d64988a2b
mick <mick@asoftware.net.au>
2013-08-06 00:34:19 +1000

    Updates to the drop db scripts

--- dropdb.bat
+++ dropdb.bat
@@ -4,6 +4,6 @@ echo Y | mysqladmin -u root -ppassword drop mft-devise-dev
 mysqladmin -u root -ppassword create mft-devise-test
 mysqladmin -u root -ppassword create mft-devise-dev
 
-rake db:migrate
-rake db:seed
-rake db:test:prepare
+bundle exec rake db:migrate
+bundle exec rake db:seed
+bundle exec rake db:test:prepare
