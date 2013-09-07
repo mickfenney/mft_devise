7e4d4cdaac9f6db49f0273e6d2a67407124fc373
mick <mick@asoftware.net.au>
2013-09-08 00:46:18 +1000

    Video updates

--- db/seeds_vids.rb
+++ db/seeds_vids.rb
@@ -3,7 +3,7 @@ if Video.count == 0
   vid = Video.create(
     :name => 'Summernats - Burnouts',
     :code => 'epKqF5hAWf4',
-    :description => 'This is a test youtube video of the sumernats burnout competition...',
+    :description => 'Summernats 25 Top Ten Burnouts 2012',
     :user_id => 1,
   )
   puts '+ vid: ' << vid.name
