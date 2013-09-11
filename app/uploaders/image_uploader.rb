07a00ed36ad8ad48e55dc6d758fcde3c91b509e2
mick <mick@asoftware.net.au>
2013-09-11 20:57:18 +1000

    Updates to the default image size of the uploaders

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -26,14 +26,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :scale => [600, 600] unless RUBY_PLATFORM =~ /mingw/i
-
+  # process :scale => [600, 600]
+  #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
   unless RUBY_PLATFORM =~ /mingw/i
+    process :resize_to_limit => [600, 600]
     version :thumb do
       process :resize_to_limit => [150, 150]
     end
