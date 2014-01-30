e2afa7dc7ef85467770db9953d5bd6ac282bea5e
mick-asoftware <mick@asoftware.net.au>
2014-01-30 14:43:32 +1100

    Added more rake tasks

--- dropdb.sh
+++ dropdb.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+echo "+ Removing log directory..."
 rm -r log
+echo "+ Removing tmp directory..."
 rm -r tmp
+echo "+ Removing public/uploads directory..."
 rm -r public/uploads
 
+echo "+ Dropping Database..."
 rake db:drop
+echo "+ Creating Database..."
 rake db:create
+echo "+ Migrating Database..."
 rake db:migrate
+echo "+ Seeding Database..."
 rake db:seed
+echo "+ Preparing test Database..."
 rake db:test:prepare
+
+echo "+ All tasks have now been completed"
