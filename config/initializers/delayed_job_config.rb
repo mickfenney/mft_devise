abd78bb9ac3740098fe6bd1b7b5b3cfe209883a1
mick-asoftware <mick@asoftware.net.au>
2014-02-10 02:21:43 +0000

    Added log rotation to delayed job

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,2 +1,7 @@
 Delayed::Worker.max_attempts = 5
 #Delayed::Worker.delay_jobs = !Rails.env.test?
+
+Delayed::Worker.logger = Logger.new("#{Rails.root}/log/delayed_job.log", 20, 1048576)
+if caller.last =~ /script\/delayed_job/ or (File.basename($0) == "rake" and ARGV[0] =~ /jobs\:work/)
+  ActiveRecord::Base.logger = Delayed::Worker.logger
+end
